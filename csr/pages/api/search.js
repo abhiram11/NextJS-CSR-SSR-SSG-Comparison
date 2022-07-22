59ebc3add66b6840d46ce8ade7871c4319a37ad6
abhiram11 <abhiramsatpute@gmail.com>
2022-07-22 16:59:36 +0530

    added a query filter based on regex to api, works fine with query q

--- csr/pages/api/search.js
+++ csr/pages/api/search.js
@@ -2,12 +2,22 @@ import pokemon from "../../pokemon.json";
 
 export default function pokeHandler(req, res) {
   //   res.status(200).json({ hello: "There" });
-
-  res.statusCode = 200;
+  const filter = req.query.q ? new RegExp(req.query.q, "i") : /.*/;
+  // "i" means case insensitive
+  //   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(
-    JSON.stringify({
-      hello: "there ",
-    })
-  );
+  res
+    .status(200)
+    .json(
+      pokemon
+        .filter(({ name: { english } }) => english.match(filter))
+        .slice(0, 10)
+    );
+  //   res.end(
+  //     JSON.stringify(
+  //       pokemon
+  //         .filter(({ name: { english } }) => english.match(filter))
+  //         .slice(0, 10)
+  //     )
+  //   );
 }
